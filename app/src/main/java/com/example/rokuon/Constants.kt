ae9b3ab9b1e6b8787d00f6cad0dd10d785233949
Tao130 <momossy5@gmail.com>
2020-10-05 22:09:36 +0900

    Constantsをオブジェクト宣言で定義する

--- app/src/main/java/com/example/rokuon/Constants.kt
+++ app/src/main/java/com/example/rokuon/Constants.kt
@@ -3,10 +3,8 @@ package com.example.rokuon
 import android.content.Context
 import android.os.Environment
 
-class Constants {
-    companion object {
+object Constants {
         val dirPath: (Context) -> String = {
             it.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath.toString()
         }
-    }
 }
