8be76979b5392272321a1dfabf1cce43b053bce7
Tao130 <momossy5@gmail.com>
2020-10-05 22:11:42 +0900

    クラス名ConstantsをRecordFileManagerに変更

--- app/src/main/java/com/example/rokuon/Constants.kt
+++ app/src/main/java/com/example/rokuon/Constants.kt
@@ -3,7 +3,7 @@ package com.example.rokuon
 import android.content.Context
 import android.os.Environment
 
-object Constants {
+object RecordFileManager {
         val dirPath: (Context) -> String = {
             it.getExternalFilesDir(Environment.DIRECTORY_MUSIC)?.absolutePath.toString()
         }
