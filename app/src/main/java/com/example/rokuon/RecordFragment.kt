149245b0b3aabcf9069d3eacac3bcdbd4dfd9a1b
Tao130 <momossy5@gmail.com>
2020-10-05 21:51:37 +0900

    AudioRecorderを別ファイルで定義する

--- app/src/main/java/com/example/rokuon/RecordFragment.kt
+++ app/src/main/java/com/example/rokuon/RecordFragment.kt
@@ -1,8 +1,6 @@
 package com.example.rokuon
 
-import android.media.MediaRecorder
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +9,6 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.rokuon.databinding.FragmentRecordBinding
-import java.io.IOException
 
 class RecordFragment : Fragment() {
     private val args: RecordFragmentArgs by navArgs()
@@ -44,42 +41,5 @@ class RecordFragment : Fragment() {
             findNavController().navigate(R.id.action_recordFragment_to_recordListFragment)
         }
         return binding.root
-    }
-}
-
-class AudioRecorder {
-
-    private var recorder: MediaRecorder? = null
-
-    fun startRecording(filePath: String) {
-        recorder = MediaRecorder().apply {
-            // 音源
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            // 出力ファイル形式
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            // 出力ファイル名
-            setOutputFile(filePath)
-            // 音声エンコーダ
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-        }
-        try {
-            // 初期化の完了
-            recorder?.prepare()
-        } catch (e: IOException) {
-            Log.e("AudioRecorder", "prepare() failed")
-        }
-        // レコーダーの開始
-        recorder?.start()
-    }
-
-    fun stopRecording() {
-        // レコーダーの停止
-        recorder?.stop()
-        // MediaRecorderインスタンスの使用を終えたらできるだけ早くリソースを解放する
-        recorder?.release()
-    }
-
-    fun releaseRecorder() {
-        recorder?.release()
     }
 }
